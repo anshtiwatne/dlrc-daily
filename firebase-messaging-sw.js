c347455353b6ecd8b07c01bf1409757a447e23a6
Ansh <ansh.tiwatne@gmail.com>
2023-08-14 22:10:48 +0530

    notification sw moved to root

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,14 +12,4 @@ const firebaseConfig = {
 }
 
 firebase.initializeApp(firebaseConfig)
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./firebase-messaging-sw.js')
-        .then(function (registration) {
-            console.log('Registration successful, scope is:', registration.scope)
-        }).catch(function (err) {
-            console.log('Service worker registration failed, error:', err)
-        })
-}
-
-const initMessaging = firebase.messaging()
+const messaging = firebase.messaging()
