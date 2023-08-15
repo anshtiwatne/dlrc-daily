81a777586428e41ffafc806b8cb1b0df641762d6
Ansh <ansh.tiwatne@gmail.com>
2023-08-15 11:38:12 +0530

    testfix

--- public/script.js
+++ public/script.js
@@ -205,10 +205,12 @@ function notifyMe() {
     messaging.onMessage((payload) => {
         payload = JSON.parse(JSON.stringify(payload))
         console.log(payload)
-        new Notification(payload.notification.title, {
-            body: payload.notification.body,
-            image: payload.notification.image
-        })
+        ServiceWorkerRegistration.showNotification(payload.notification.title,
+            {
+                body: payload.notification.body,
+                image: payload.notification.image
+            }
+        )
     })
 }
 
