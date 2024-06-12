52ca5e499b2dede52b20e82adedab93d6b9587bd
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2024-06-12 17:42:18 +0530

    format code

--- public/sw.js
+++ public/sw.js
@@ -19,7 +19,9 @@ self.addEventListener('install', async (event) => {
 	)
 })
 
-if (workbox.navigationPreload.isSupported()) workbox.navigationPreload.enable()
+if (workbox.navigationPreload.isSupported()) {
+	workbox.navigationPreload.enable()
+}
 
 self.addEventListener('fetch', (event) => {
 	if (event.request.mode === 'navigate') {
@@ -28,14 +30,15 @@ self.addEventListener('fetch', (event) => {
 				try {
 					const preloadResp = await event.preloadResponse
 
-					if (preloadResp) return preloadResp
-					const networkResp = await fetch(event.request)
+					if (preloadResp) {
+						return preloadResp
+					}
 
+					const networkResp = await fetch(event.request)
 					return networkResp
 				} catch (error) {
 					const cache = await caches.open(CACHE)
 					const cachedResp = await cache.match(offlineFallbackPage)
-
 					return cachedResp
 				}
 			})(),
